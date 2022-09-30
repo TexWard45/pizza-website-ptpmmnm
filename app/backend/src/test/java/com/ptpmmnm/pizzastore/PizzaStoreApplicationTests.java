fdebb4c9e8f193d5b36e633f533b6ffa53f619e5
phongdbt <phongdbt@runsystem.net>
2022-09-30 10:32:29 +0700

    Create: Core Source Backend

---
+++ app/backend/src/test/java/com/ptpmmnm/pizzastore/PizzaStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ptpmmnm.pizzastore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzaStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
