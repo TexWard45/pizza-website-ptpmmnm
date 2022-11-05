af940bc0e9a9c9cb7737659ae1e092ae89a65f45
sang <sangphan005@gmail.com>
2022-11-05 16:21:30 +0700

    Update CategoryRepository.java

--- app/backend/src/main/java/com/openedsource/pizzastore/database/repository/CategoryRepository.java
+++ app/backend/src/main/java/com/openedsource/pizzastore/database/repository/CategoryRepository.java
@@ -2,6 +2,8 @@ package com.openedsource.pizzastore.database.repository;
 
 import com.openedsource.pizzastore.database.entity.CategoryEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CategoryRepository extends JpaRepository<CategoryEntity,Integer> {
 }
