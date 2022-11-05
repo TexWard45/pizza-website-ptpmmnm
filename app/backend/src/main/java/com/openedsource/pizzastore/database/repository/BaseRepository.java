644c35a9c5edbca9b2b0d68653c57b2dc9dbb661
sang <sangphan005@gmail.com>
2022-11-05 09:13:58 +0700

    category
    delete base

--- app/backend/src/main/java/com/openedsource/pizzastore/database/repository/BaseRepository.java
+++
@@ -1,7 +0,0 @@
-package com.openedsource.pizzastore.database.repository;
-
-import com.openedsource.pizzastore.database.entity.Base;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BaseRepository extends JpaRepository<Base,Integer> {
-}
