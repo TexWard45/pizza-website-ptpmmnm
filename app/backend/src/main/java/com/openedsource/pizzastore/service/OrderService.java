ee7ea14128c17e1480ca888979d951dc263e1084
sang <sangphan005@gmail.com>
2022-11-06 16:00:34 +0700

    Order

--- app/backend/src/main/java/com/openedsource/pizzastore/service/OrderService.java
+++ app/backend/src/main/java/com/openedsource/pizzastore/service/OrderService.java
@@ -10,7 +10,7 @@ import java.util.Optional;
 @Service
 public class OrderService {
 
-    OrderRepository orderRepository;
+    private OrderRepository orderRepository;
 
     public void insertOrder(OrderEntity orderEntity) {
 
