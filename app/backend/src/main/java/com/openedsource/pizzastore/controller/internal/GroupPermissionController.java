2d9e42523046d9afac33f894e9cae191d5000d94
sang <sangphan005@gmail.com>
2022-11-06 15:59:46 +0700

    Update GroupPermissionController.java

--- app/backend/src/main/java/com/openedsource/pizzastore/controller/internal/GroupPermissionController.java
+++ app/backend/src/main/java/com/openedsource/pizzastore/controller/internal/GroupPermissionController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("internal")
+@RequestMapping("/internal")
 @CrossOrigin(origins = "*")
 public class GroupPermissionController {
     @Autowired
